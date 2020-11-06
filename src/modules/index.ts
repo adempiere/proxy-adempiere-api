9500211968a2bac9b6d44be696cec2d49fb5534b
Yamel Senih <ysenih@erpya.com>
2020-11-06 14:06:21 -0400

    Remove default magento lauch

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -9,16 +9,16 @@ import {StorefrontApiModule} from '@storefront-api/lib/module'
 import * as magento2 from '@storefront-api/platform-magento2'
 
 export let modules: StorefrontApiModule[] = [
-  DefaultVuestorefrontApiModule({
-    platform: {
-      name: 'magento2',
-      platformImplementation: magento2
-    }
-  }),
-  DefaultCatalogModule(),
-  DefaultImgModule(),
-  SampleApiModule,
-  TemplateModule,
+  // DefaultVuestorefrontApiModule({
+  //   platform: {
+  //     name: 'magento2',
+  //     platformImplementation: magento2
+  //   }
+  // }),
+  // DefaultCatalogModule(),
+  // DefaultImgModule(),
+  // SampleApiModule,
+  // TemplateModule,
   ADempiereApi,
   ADempiereStore
 ]
