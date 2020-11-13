902bf84413512c0464d7c21d3cb1c86521349f9e
Yamel Senih <ysenih@erpya.com>
2020-11-13 15:56:19 -0400

    Add default catalog for elastic search based

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,21 +1,21 @@
 import { DefaultVuestorefrontApiModule } from '@storefront-api/default-vsf'
 import { DefaultCatalogModule } from '@storefront-api/default-catalog'
-import { DefaultImgModule } from '@storefront-api/default-img'
-import { SampleApiModule } from './sample-api'
+// import { DefaultImgModule } from '@storefront-api/default-img'
+// import { SampleApiModule } from './sample-api'
 import { ADempiereApi } from './adempiere-api'
 import { ADempiereStore } from './adempiere-store'
-import { TemplateModule } from './template-module'
+// import { TemplateModule } from './template-module'
 import {StorefrontApiModule} from '@storefront-api/lib/module'
 import * as magento2 from '@storefront-api/platform-magento2'
 
 export let modules: StorefrontApiModule[] = [
-  // DefaultVuestorefrontApiModule({
-  //   platform: {
-  //     name: 'magento2',
-  //     platformImplementation: magento2
-  //   }
-  // }),
-  // DefaultCatalogModule(),
+  DefaultVuestorefrontApiModule({
+    platform: {
+      name: 'magento2',
+      platformImplementation: magento2
+    }
+  }),
+  DefaultCatalogModule(),
   // DefaultImgModule(),
   // SampleApiModule,
   // TemplateModule,
