31364c8e939812b5367eedc0ae39ff4f5809df63
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2021-02-25 13:15:41 -0400

    fix: Run process SB record selection. (#11)
    * fix: Run process SB record selection.
    
    * Update package.json
    
    Co-authored-by: EdwinBetanc0urt <EdwinBetanco0urt@outlook.com>

--- src/modules/adempiere-api/index.ts
+++ src/modules/adempiere-api/index.ts
@@ -1,36 +1,39 @@
-import { StorefrontApiModule, registerExtensions } from '@storefront-api/lib/module'
-import { StorefrontApiContext } from '@storefront-api/lib/module/types'
+import { StorefrontApiModule, registerExtensions } from '@storefront-api/lib/module';
+import { StorefrontApiContext } from '@storefront-api/lib/module/types';
 import { Router } from 'express';
 import user from './api/user';
 import enrollment from './api/enrollment';
-import img from './api/img'
-import resource from './api/resource'
-import dictionary from './api/dictionary'
-import userInterface from './api/userInterface'
-import userLogs from './api/userLogs'
-import workflow from './api/workflow'
-import dashboard from './api/dashboard'
-import data from './api/data'
-import pos from './api/pos'
-import core from './api/core'
+import img from './api/img';
+import resource from './api/resource';
+import dictionary from './api/dictionary';
+import userInterface from './api/userInterface';
+import userLogs from './api/userLogs';
+import workflow from './api/workflow';
+import dashboard from './api/dashboard';
+import data from './api/data';
+import pos from './api/pos';
+import core from './api/core';
 import { version } from '../../../package.json';
-import path from 'path'
+import path from 'path';
 
 export const ADempiereApi: StorefrontApiModule = new StorefrontApiModule({
   key: 'adempiere-api',
   initApi: ({ config, db, app }: StorefrontApiContext): void => {
-    let api = Router();
-    const Api = require('@adempiere/grpc-api')
-    // console.log('Config: ', config)
-    let service = new Api(config)
-    service.initService()
-    // mount the user resource
+    const api = Router();
+    const Api = require('@adempiere/grpc-api');
+    const service = new Api(config);
+    service.initService();
+
+    // Mount the services
+
+    // User (or access)
     api.use('/user', user({ config, db, service }));
-    //  Enrollment service
+    //  Enrollment
     api.use('/enrollment', enrollment({ config, db, service }));
+    // Image
     api.use('/img', img({ config, db, service }));
     api.use('/img/:width/:height/:action/:image', (req, res, next) => {
-      console.log(req.params)
+      console.log(req.params);
     });
     //  Resource
     api.use('/resource', resource({ config, db, service }));
@@ -50,15 +53,23 @@ export const ADempiereApi: StorefrontApiModule = new StorefrontApiModule({
     api.use('/core', core({ config, db, service }));
     //  POS
     api.use('/pos', pos({ config, db, service }));
+
+    // version
     // perhaps expose some API metadata at the root
     api.get('/', (req, res) => {
       res.json({ version });
     });
 
-    registerExtensions({ app, config, db, registeredExtensions: config.get('modules.adempiereApi.registeredExtensions'), rootPath: path.join(__dirname, 'api', 'extensions') })
+    registerExtensions({
+      app,
+      config,
+      db,
+      registeredExtensions: config.get('modules.adempiereApi.registeredExtensions'),
+      rootPath: path.join(__dirname, 'api', 'extensions')
+    });
 
     // api router
     app.use('/adempiere-api', api);
   }
 
-})
+});
