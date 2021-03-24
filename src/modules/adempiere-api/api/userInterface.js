07758677d46e30751e784505f0fa5d608bcea953
Yamel Senih <ysenih@erpya.com>
2021-03-24 14:53:16 -0400

    Add support to services: - set-preference - delete-preference

--- src/modules/adempiere-api/api/userInterface.js
+++ src/modules/adempiere-api/api/userInterface.js
@@ -15,7 +15,8 @@ import {
   convertChatEntryFromGRPC,
   convertPrivateAccessFromGRPC,
   convertLookupFromGRPC,
-  convertCalloutFromGRPC
+  convertCalloutFromGRPC,
+  convertPreferenceFromGRPC
 } from '@adempiere/grpc-api/lib/convertBusinessData';
 export default ({ config, db, service }) => {
   let userInterfaceApi = Router();
@@ -75,6 +76,88 @@ export default ({ config, db, service }) => {
           res.json({
             code: 200,
             result: convertResourceReferenceFromGRPC(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  /**
+   * POST Set User Preference
+   *
+   * req.query.token - user token
+   * req.query.language - login language
+   * req.body.container_uuid - Container UUID, usually is a window
+   * req.body.column_name - attribute or column name
+   * req.body.is_for_current_user  - apply for current user (boolean)
+   * req.body.is_for_current_client - apply for current client (boolean)
+   * req.body.is_for_current_organization - apply for current organization (boolean)
+   * req.body.is_for_current_container - apply for current container (boolean)
+   * req.body.value - value for set on preference
+   *
+   */
+  userInterfaceApi.post('/set-preference', (req, res) => {
+    if (req.body) {
+      service.setPreference({
+        token: req.query.token,
+        language: req.query.language,
+        containerUuid: req.body.container_uuid,
+        columnName: req.body.column_name,
+        isForCurrentUser: req.body.is_for_current_user,
+        isForCurrentClient: req.body.is_for_current_client,
+        isForCurrentOrganization: req.body.is_for_current_organization,
+        isForCurrentContainer: req.body.is_for_current_container,
+        value: req.body.value
+      }, function (err, response) {
+        if (response) {
+          res.json({
+            code: 200,
+            result: convertPreferenceFromGRPC(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  /**
+   * POST Delete User Preference
+   *
+   * req.query.token - user token
+   * req.query.language - login language
+   * req.body.container_uuid - Container UUID, usually is a window
+   * req.body.column_name - attribute or column name
+   * req.body.is_for_current_user  - apply for current user (boolean)
+   * req.body.is_for_current_client - apply for current client (boolean)
+   * req.body.is_for_current_organization - apply for current organization (boolean)
+   * req.body.is_for_current_container - apply for current container (boolean)
+   *
+   */
+  userInterfaceApi.post('/delete-preference', (req, res) => {
+    if (req.body) {
+      service.deletePreference({
+        token: req.query.token,
+        language: req.query.language,
+        containerUuid: req.body.container_uuid,
+        columnName: req.body.column_name,
+        isForCurrentUser: req.body.is_for_current_user,
+        isForCurrentClient: req.body.is_for_current_client,
+        isForCurrentOrganization: req.body.is_for_current_organization,
+        isForCurrentContainer: req.body.is_for_current_container
+      }, function (err, response) {
+        if (response) {
+          res.json({
+            code: 200,
+            result: 'Ok'
           })
         } else if (err) {
           res.json({
