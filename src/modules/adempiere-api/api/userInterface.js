d3cd83f1a544d72edfc96e01f1779ddaa0bccc4b
Yamel Senih <ysenih@erpya.com>
2021-04-13 15:27:49 -0400

    Add support to services: - get-record-access - set-record-access

--- src/modules/adempiere-api/api/userInterface.js
+++ src/modules/adempiere-api/api/userInterface.js
@@ -16,7 +16,8 @@ import {
   convertPrivateAccessFromGRPC,
   convertLookupFromGRPC,
   convertCalloutFromGRPC,
-  convertPreferenceFromGRPC
+  convertPreferenceFromGRPC,
+  convertRecordAccessFromGRPC
 } from '@adempiere/grpc-api/lib/convertBusinessData';
 export default ({ config, db, service }) => {
   let userInterfaceApi = Router();
@@ -158,6 +159,103 @@ export default ({ config, db, service }) => {
           res.json({
             code: 200,
             result: 'Ok'
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  /**
+   * GET Entity Record Access
+   *
+   * req.query.token - user token
+   * req.query.id - id of entity
+   * req.query.uuid - uuid of entity
+   * req.query.table_name - table name of entity
+   * req.query.language - login language
+   *
+   * Details: https://sfa-docs.now.sh/guide/default-modules/api.html#get-vsbridgeuserorder-history
+   */
+  userInterfaceApi.get('/record-access', (req, res) => {
+    if (req.query) {
+      service.getRecordAccess({
+        token: req.query.token,
+        language: req.query.language,
+        tableName: req.query.table_name,
+        id: req.query.id,
+        uuid: req.query.uuid
+      }, function (err, response) {
+        if (response) {
+          res.json({
+            code: 200,
+            result: convertRecordAccessFromGRPC(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  /**
+   * POST Set Record Access
+   *
+   * req.query.token - user token
+   * req.body.id - id of entity
+   * req.body.uuid - uuid of entity
+   * req.body.table_name - table name of entity
+   * req.body.record_accesses - list of access by role
+   * record_accesses: [{
+   *  role_id,
+   *  role_uuid,
+   *  role_name,
+   *  is_active,
+   *  is_exclude,
+   *  is_read_only,
+   *  is_dependent_entities
+   * }]
+   * req.query.language - login language
+   *
+   */
+  userInterfaceApi.post('/set-record-access', (req, res) => {
+    if (req.body) {
+      //  Validate record access
+      let recordAccesses = []
+      if (req.body.record_accesses) {
+        let record = {}
+        req.body.record_accesses.map(accessToSet => {
+          record = {
+            roleId: accessToSet.role_id,
+            roleUuid: accessToSet.role_uuid,
+            roleName: accessToSet.role_name,
+            isActive: accessToSet.is_active,
+            isExclude: accessToSet.is_exclude,
+            isReadOnly: accessToSet.is_read_only,
+            isDependentEntities: accessToSet.is_dependent_entities
+          }
+        })
+        recordAccesses.push(record)
+      }
+      service.setRecordAccess({
+        token: req.query.token,
+        language: req.query.language,
+        id: req.body.id,
+        uuid: req.body.uuid,
+        tableName: req.body.table_name,
+        recordAccesses: recordAccesses
+      }, function (err, response) {
+        if (response) {
+          res.json({
+            code: 200,
+            result: convertRecordAccessFromGRPC(response)
           })
         } else if (err) {
           res.json({
