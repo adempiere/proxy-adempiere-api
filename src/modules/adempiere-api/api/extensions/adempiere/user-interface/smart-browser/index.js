3b614797f7e93e2a23f4067bb185aa629c34014b
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2021-06-03 19:37:26 -0400

    fix: Browser Items. (#25)
    Co-authored-by: EdwinBetanc0urt <EdwinBetanco0urt@outlook.com>

--- src/modules/adempiere-api/api/extensions/adempiere/user-interface/smart-browser/index.js
+++ src/modules/adempiere-api/api/extensions/adempiere/user-interface/smart-browser/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import {
   convertEntityFromGRPC
 } from '@adempiere/grpc-api/lib/convertBaseDataType';
-module.exports = ({ config, db }) => {
+module.exports = ({ config }) => {
   let api = Router();
   const ServiceApi = require('@adempiere/grpc-api')
   let service = new ServiceApi(config)
@@ -15,58 +15,57 @@ module.exports = ({ config, db }) => {
    * req.query.language - login language
    * req.query.page_size - size of page (customized)
    * req.query.page_token - token of page (optional for get a specific page)
-   * req.query.uuid - browser uuid
-   * req.query.parameters - parameters of browser
-   * req.query.filters - query filters
-   * req.query.columns - query columns
-   * req.query.table_name - table name (Mandatory if is not a query)
-   * req.query.query - custom query instead a table name based on SQL
-   * req.query.where_clause - where clause of search based on SQL
-   * req.query.order_by_clause - order by clause based on SQL
-   * req.query.limit - records limit
+   * req.body.uuid - browser uuid
+   * req.body.filters - query filters
+   * req.body.table_name - table name (Mandatory if is not a query)
+   * req.body.query - custom query instead a table name based on SQL
+   * req.body.where_clause - where clause of search based on SQL
+   * req.body.order_by_clause - order by clause based on SQL
+   * req.body.limit - records limit
    *
    * Details:
    */
-  api.get('/browser-items', (req, res) => {
+  api.post('/browser-items', (req, res) => {
+    const browserRequest = {};
     if (req.query) {
-      service.listBrowserItems({
-        token: req.query.token,
-        language: req.query.language,
-        //  Running parameters
-        uuid: req.query.uuid,
-        parameters: req.query.parameters,
-        tableName: req.query.table_name,
-        //  DSL Query
-        filters: req.query.filters,
-        columns: req.query.columns,
-        //  Custom Query
-        query: req.query.query,
-        whereClause: req.query.where_clause,
-        orderByClause: req.query.order_by_clause,
-        limit: req.query.limit,
-        //  Page Data
-        pageSize: req.query.page_size,
-        pageToken: req.query.page_token
-      }, function (err, response) {
-        if (response) {
-          res.json({
-            code: 200,
-            result: {
-              record_count: response.getRecordCount(),
-              next_page_token: response.getNextPageToken(),
-              records: response.getRecordsList().map(entity => {
-                return convertEntityFromGRPC(entity)
-              })
-            }
-          })
-        } else if (err) {
-          res.json({
-            code: 500,
-            result: err.details
-          })
-        }
-      })
+      browserRequest.token = req.query.token;
+      browserRequest.language = req.query.language;
+      //  Page Data
+      browserRequest.pageSize = req.query.page_size;
+      browserRequest.pageToken = req.query.page_token;
     }
+    if (req.body) {
+      //  Running parameters
+      browserRequest.uuid = req.body.uuid;
+      browserRequest.tableName = req.body.table_name;
+      //  DSL Query
+      browserRequest.filters = req.body.filters;
+      //  Custom Query
+      browserRequest.query = req.body.query;
+      browserRequest.whereClause = req.body.where_clause;
+      browserRequest.orderByClause = req.body.order_by_clause;
+      browserRequest.limit = req.body.limit;
+    }
+
+    service.listBrowserItems(browserRequest, (err, response) => {
+      if (response) {
+        res.json({
+          code: 200,
+          result: {
+            record_count: response.getRecordCount(),
+            next_page_token: response.getNextPageToken(),
+            records: response.getRecordsList().map(entity => {
+              return convertEntityFromGRPC(entity)
+            })
+          }
+        });
+      } else if (err) {
+        res.json({
+          code: 500,
+          result: err.details
+        });
+      }
+    });
   });
 
   return api;
