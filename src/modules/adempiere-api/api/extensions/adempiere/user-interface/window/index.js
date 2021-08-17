9fd961f185f7165e191405749c3be0a9c9f22383
Yamel Senih <ysenih@erpya.com>
2021-08-17 12:29:31 -0400

    Add service: user-interface/window/entites

--- src/modules/adempiere-api/api/extensions/adempiere/user-interface/window/index.js
+++ src/modules/adempiere-api/api/extensions/adempiere/user-interface/window/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import {
   convertRecordReferenceInfoFromGRPC,
-  convertValueFromGRPC
+  convertValueFromGRPC,
+  convertEntityFromGRPC
 } from '@adempiere/grpc-api/lib/convertBaseDataType';
 import {
   convertLookupFromGRPC,
@@ -285,6 +286,61 @@ module.exports = ({ config, db }) => {
           res.json({
             code: 200,
             result: convertCalloutFromGRPC(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  /**
+   * GET Entities, used for window list
+   *
+   * req.query.token - user token
+   * req.query.language - login language
+   * req.query.page_size - size of page (customized)
+   * req.query.page_token - token of page (optional for get a specific page)
+   * req.query.filters - query filters
+   * req.query.table_name - table name (Mandatory if is not a query)
+   * req.query.query - custom query instead a table name based on SQL
+   * req.query.where_clause - where clause of search based on SQL
+   * req.query.order_by_clause - order by clause based on SQL
+   * req.query.limit - records limit
+   *
+   * Details:
+   */
+  api.get('/entities', (req, res) => {
+    if (req.query) {
+      service.listTabEntities({
+        token: req.query.token,
+        language: req.query.language,
+        //  Running parameters
+        windowUuid: req.query.window_uuid,
+        tabUuid: req.query.tab_uuid,
+        windowNo: req.query.window_no,
+        //  DSL Query
+        filters: req.query.filters,
+        //  Custom Query
+        sorting: req.query.sorting,
+        attributes: req.query.attributes,
+        //  Page Data
+        pageSize: req.query.page_size,
+        pageToken: req.query.page_token
+      }, function (err, response) {
+        if (response) {
+          res.json({
+            code: 200,
+            result: {
+              record_count: response.getRecordCount(),
+              next_page_token: response.getNextPageToken(),
+              records: response.getRecordsList().map(entity => {
+                return convertEntityFromGRPC(entity);
+              })
+            }
           })
         } else if (err) {
           res.json({
