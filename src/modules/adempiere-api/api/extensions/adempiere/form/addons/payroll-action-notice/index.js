3802d81f7bbe87a3662a69ec63be93cc737f19d4
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-08-05 10:26:00 -0400

    feat: Add payroll action notice services. (#16)

---
+++ src/modules/adempiere-api/api/extensions/adempiere/form/addons/payroll-action-notice/index.js
@@ -0,0 +1,208 @@
+/************************************************************************************
+ * Copyright (C) 2012-2022 E.R.P. Consultores y Asociados, C.A.                     *
+ * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                     *
+ * This program is free software: you can redistribute it and/or modify             *
+ * it under the terms of the GNU General Public License as published by             *
+ * the Free Software Foundation, either version 2 of the License, or                *
+ * (at your option) any later version.                                              *
+ * This program is distributed in the hope that it will be useful,                  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                     *
+ * GNU General Public License for more details.                                     *
+ * You should have received a copy of the GNU General Public License                *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
+
+import { Router } from 'express';
+
+import { convertEntitiesListFromGRPC } from '../../../util/convertData';
+import {
+  convertEntityFromGRPC
+} from '@adempiere/grpc-api/lib/convertBaseDataType';
+import { convertLookupFromGRPC } from '@adempiere/grpc-api/lib/convertBusinessData';
+
+module.exports = ({ config }) => {
+  const api = Router();
+  const ServiceApi = require('@adempiere/grpc-api/src/services/payrollActionNotice')
+  const service = new ServiceApi(config);
+
+  api.get('/list-payroll-process', (req, res) => {
+    if (req.query) {
+      service.listPayrollProcess({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        contextAttributes: req.query.context_attributes,
+        searchValue: req.query.search_value,
+        //  Page Data
+        pageSize: req.query.page_size,
+        pageToken: req.query.page_token
+      }, function (err, response) {
+        if (response) {
+          res.json({
+            code: 200,
+            result: {
+              record_count: response.getRecordCount(),
+              next_page_token: response.getNextPageToken(),
+              records: response.getRecordsList().map(lookupItem => {
+                return convertLookupFromGRPC(lookupItem)
+              })
+            }
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  api.get('/list-employee-valid', (req, res) => {
+    if (req.query) {
+      service.listEmployeeValid({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        contextAttributes: req.query.context_attributes,
+        searchValue: req.query.search_value,
+        //  Page Data
+        pageSize: req.query.page_size,
+        pageToken: req.query.page_token
+      }, function (err, response) {
+        if (response) {
+          res.json({
+            code: 200,
+            result: {
+              record_count: response.getRecordCount(),
+              next_page_token: response.getNextPageToken(),
+              records: response.getRecordsList().map(lookupItem => {
+                return convertLookupFromGRPC(lookupItem)
+              })
+            }
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  api.get('/list-payroll-concepts', (req, res) => {
+    if (req.query) {
+      service.listPayrollConcepts({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        contextAttributes: req.query.context_attributes,
+        searchValue: req.query.search_value,
+        //  Page Data
+        pageSize: req.query.page_size,
+        pageToken: req.query.page_token
+      }, (err, response) => {
+        if (response) {
+          res.json({
+            code: 200,
+            result: {
+              record_count: response.getRecordCount(),
+              next_page_token: response.getNextPageToken(),
+              records: response.getRecordsList().map(lookupItem => {
+                return convertLookupFromGRPC(lookupItem)
+              })
+            }
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  api.get('/list-payroll-movements', (req, res) => {
+    if (req.query) {
+      service.listPayrollMovements({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        filters: req.query.filters,
+        contextAttributes: req.query.context_attributes,
+        searchValue: req.query.search_value,
+        //  Page Data
+        pageSize: req.query.page_size,
+        pageToken: req.query.page_token
+      }, (err, response) => {
+        if (response) {
+          res.json({
+            code: 200,
+            result: convertEntitiesListFromGRPC(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  api.get('/save-payroll-movement', (req, res) => {
+    if (req.query) {
+      service.savePayrollMovement({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        id: req.query.id,
+        uuid: req.query.uuid,
+        contextAttributes: req.query.context_attributes,
+        attributes: req.query.attributes
+      }, (err, response) => {
+        if (response) {
+          res.json({
+            code: 200,
+            result: convertEntityFromGRPC(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  api.get('/delete-payroll-movements', (req, res) => {
+    if (req.query) {
+      service.deletePayrollMovements({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        contextAttributes: req.query.context_attributes,
+        ids: req.query.ids,
+        uuids: req.query.uuids
+      }, (err, response) => {
+        if (response) {
+          res.json({
+            code: 200,
+            result: 'Ok'
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  return api;
+};
