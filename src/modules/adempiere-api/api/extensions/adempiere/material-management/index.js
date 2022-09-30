ed864c11aa7971b720099226a17591cf4f2d216c
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-09-30 10:49:17 -0400

    feat: Product Storage Info.

--- src/modules/adempiere-api/api/extensions/adempiere/material-management/index.js
+++ src/modules/adempiere-api/api/extensions/adempiere/material-management/index.js
@@ -1,8 +1,20 @@
+/************************************************************************************
+ * Copyright (C) 2012-2022 E.R.P. Consultores y Asociados, C.A.                     *
+ * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                     *
+ * This program is free software: you can redistribute it and/or modify             *
+ * it under the terms of the GNU General Public License as published by             *
+ * the Free Software Foundation, either version 2 of the License, or                *
+ * (at your option) any later version.                                              *
+ * This program is distributed in the hope that it will be useful,                  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                     *
+ * GNU General Public License for more details.                                     *
+ * You should have received a copy of the GNU General Public License                *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
+
 import { Router } from 'express';
 import { convertEntitiesListFromGRPC } from '../util/convertData';
-import {
-  convertEntityFromGRPC
-} from '@adempiere/grpc-api/lib/convertBaseDataType';
 
 module.exports = ({ config }) => {
   const api = Router();
@@ -20,7 +32,7 @@ module.exports = ({ config }) => {
    */
   api.get('/list-product-storage', (req, res) => {
     if (req.query) {
-      service.listProductStorageRequest({
+      service.listProductStorage({
         token: req.query.token,
         language: req.query.language,
         //  DSL Query
