08bfcab39075ba05a01cb0f4aedf77ecf0526c4f
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2022-10-31 12:21:50 -0400

    feat: List Product Attribute Instances. (#62)
    * feat: List Product Attribute Instances.
    
    * feat: Add get Product Attribute Set.
    
    * update package.json

--- src/modules/adempiere-api/api/extensions/adempiere/material-management/index.js
+++ src/modules/adempiere-api/api/extensions/adempiere/material-management/index.js
@@ -15,6 +15,10 @@
 
 import { Router } from 'express';
 import { convertEntitiesListFromGRPC } from '../util/convertData';
+import {
+  convertProductAttributeSet,
+  convertListProductAttributeSetInstances
+} from '../util/convertMaterialManagement';
 
 module.exports = ({ config }) => {
   const api = Router();
@@ -22,7 +26,7 @@ module.exports = ({ config }) => {
   const service = new ServiceApi(config);
 
   /**
-   * GET List Accounting Combinations
+   * GET List Product Storage
    *
    * req.query.token - user token
    * req.query.language - login language
@@ -47,6 +51,82 @@ module.exports = ({ config }) => {
           res.json({
             code: 200,
             result: convertEntitiesListFromGRPC(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  /**
+   * POST List Product Attribute Set Instances
+   *
+   * req.query.token - user token
+   * req.query.language - login language
+   * req.query.search_value - search value optional
+   * req.query.context_attributes - attributes
+   * req.query.filters - filters to reduce list values
+   */
+  api.post('/get-product-attribute-set', (req, res) => {
+    if (req.body) {
+      service.getProductAttributeSet({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        id: req.body.id,
+        uuid: req.body.uuid,
+        productId: req.body.product_id,
+        productUuid: req.body.product_uuid,
+        productAttributeSetInstanceId: req.body.product_attribute_set_instance_id,
+        productAttributeSetInstanceUuid: req.body.product_attribute_set_instance_uuid
+      }, (err, response) => {
+        if (response) {
+          res.json({
+            code: 200,
+            result: convertProductAttributeSet(response)
+          })
+        } else if (err) {
+          res.json({
+            code: 500,
+            result: err.details
+          })
+        }
+      })
+    }
+  });
+
+  /**
+   * POST List Product Attribute Set Instances
+   *
+   * req.query.token - user token
+   * req.query.language - login language
+   * req.query.search_value - search value optional
+   * req.query.context_attributes - attributes
+   * req.query.filters - filters to reduce list values
+   */
+  api.post('/list-product-attribute-set-instances', (req, res) => {
+    if (req.body) {
+      service.listProductAttributeSetInstances({
+        token: req.query.token,
+        language: req.query.language,
+        //  DSL Query
+        filters: req.body.filters,
+        productId: req.body.product_id,
+        productUuid: req.body.product_uuid,
+        productAttributeSetId: req.body.product_attribute_set_id,
+        productAttributeSetUuid: req.body.product_attribute_set_uuid,
+        //  Page Data
+        pageSize: req.query.page_size,
+        pageToken: req.query.page_token
+      }, (err, response) => {
+        if (response) {
+          res.json({
+            code: 200,
+            result: convertListProductAttributeSetInstances(response)
           })
         } else if (err) {
           res.json({
---
+++ src/modules/adempiere-api/api/extensions/adempiere/util/convertMaterialManagement.js
@@ -0,0 +1,110 @@
+/************************************************************************************
+ * Copyright (C) 2012-2022 E.R.P. Consultores y Asociados, C.A.                     *
+ * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                     *
+ * This program is free software: you can redistribute it and/or modify             *
+ * it under the terms of the GNU General Public License as published by             *
+ * the Free Software Foundation, either version 2 of the License, or                *
+ * (at your option) any later version.                                              *
+ * This program is distributed in the hope that it will be useful,                  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                     *
+ * GNU General Public License for more details.                                     *
+ * You should have received a copy of the GNU General Public License                *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
+
+export function convertProductAttributeValue (productAttributeValue) {
+  if (!productAttributeValue) {
+    return undefined
+  }
+
+  return {
+    id: productAttributeValue.getId(),
+    uuid: productAttributeValue.getUuid(),
+    value: productAttributeValue.getValue(),
+    name: productAttributeValue.getName(),
+    description: productAttributeValue.getDescription()
+  }
+}
+
+export function convertProductAttribute (productAttribute) {
+  if (!productAttribute) {
+    return undefined
+  }
+
+  return {
+    id: productAttribute.getId(),
+    uuid: productAttribute.getUuid(),
+    name: productAttribute.getName(),
+    description: productAttribute.getDescription(),
+    value_type: productAttribute.getValueType(),
+    is_mandatory: productAttribute.getIsMandatory(),
+    is_instance_attribute: productAttribute.getIsInstanceAttribute(),
+    sequence: productAttribute.getSequence(),
+    product_attribute_values: productAttribute.getProductAttributeValuesList().map(productAttributeValue => {
+      return convertProductAttributeValue(productAttributeValue)
+    })
+  }
+}
+
+export function convertProductAttributeSet (productAttributeSet) {
+  if (!productAttributeSet) {
+    return undefined;
+  }
+
+  return {
+    id: productAttributeSet.getId(),
+    uuid: productAttributeSet.getUuid(),
+    name: productAttributeSet.getName(),
+    description: productAttributeSet.getDescription(),
+    is_instance_attribute: productAttributeSet.getIsInstanceAttribute(),
+    is_lot: productAttributeSet.getIsLot(),
+    is_lot_mandatory: productAttributeSet.getIsLotMandatory(),
+    lot_control_id: productAttributeSet.getLotControlId(),
+    lot_char_start_overwrite: productAttributeSet.getLotCharStartOverwrite(),
+    lot_char_end_overwrite: productAttributeSet.getLotCharEndOverwrite(),
+    // Serial Attributes
+    is_serial: productAttributeSet.getIsSerial(),
+    is_serial_mandatory: productAttributeSet.getIsSerialMandatory(),
+    serial_control_id: productAttributeSet.getSerialControlId(),
+    serial_char_start_overwrite: productAttributeSet.getSerialCharStartOverwrite(),
+    serial_char_end_overwrite: productAttributeSet.getSerialCharEndOverwrite(),
+    // Guarantee Date Attributes
+    is_guarantee_date: productAttributeSet.getIsGuaranteeDate(),
+    is_guarantee_date_mandatory: productAttributeSet.getIsGuaranteeDateMandatory(),
+    guarantee_days: productAttributeSet.getGuaranteeDays(),
+    madatory_type: productAttributeSet.getMandatoryType(),
+    product_attributes: productAttributeSet.getProductAttributesList().map(productAttribute => {
+      return convertProductAttribute(productAttribute)
+    })
+  }
+}
+
+export function convertProductAttributeSetInstance (productAttributeSetInstance) {
+  if (!productAttributeSetInstance) {
+    return undefined
+  }
+
+  return {
+    id: productAttributeSetInstance.getId(),
+    uuid: productAttributeSetInstance.getUuid(),
+    description: productAttributeSetInstance.getDescription(),
+    guarantee_date: productAttributeSetInstance.getGuaranteeDate(),
+    lot: productAttributeSetInstance.getLot(),
+    lot_id: productAttributeSetInstance.getLotId(),
+    serial: productAttributeSetInstance.getSerial(),
+    product_attribute_set: convertProductAttributeSet(
+      productAttributeSetInstance.getProductAttributeSet()
+    )
+  }
+}
+
+export function convertListProductAttributeSetInstances (productAttributeSetInstancesList) {
+  return {
+    record_count: productAttributeSetInstancesList.getRecordCount(),
+    next_page_token: productAttributeSetInstancesList.getNextPageToken(),
+    records: productAttributeSetInstancesList.getRecordsList().map(productAttributeSetInstance => {
+      return convertProductAttributeSetInstance(productAttributeSetInstance);
+    })
+  }
+}
