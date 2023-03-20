606555d2a83ae5a6e2268a973a4e7f8dc3830343
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-03-20 15:49:26 -0400

    fix: Undefined getMetadata method. (#111)

--- src/modules/adempiere-api/api/extensions/adempiere/user-interface/process/index.js
+++ src/modules/adempiere-api/api/extensions/adempiere/user-interface/process/index.js
@@ -1,14 +1,31 @@
+/************************************************************************************
+ * Copyright (C) 2012-2023 E.R.P. Consultores y Asociados, C.A.                     *
+ * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                     *
+ * This program is free software: you can redistribute it and/or modify             *
+ * it under the terms of the GNU General Public License as published by             *
+ * the Free Software Foundation, either version 2 of the License, or                *
+ * (at your option) any later version.                                              *
+ * This program is distributed in the hope that it will be useful,                  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                     *
+ * GNU General Public License for more details.                                     *
+ * You should have received a copy of the GNU General Public License                *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.            *
+ ************************************************************************************/
+
 import { Router } from 'express';
+
 import {
   convertReportOutputFromGRPC,
   convertDrillTableFromGRPC,
   convertPrintFromatFromGRPC,
   convertReportViewFromGRPC
 } from '@adempiere/grpc-api/lib/convertBaseDataType';
+
 module.exports = ({ config, db }) => {
-  let api = Router();
-  const ServiceApi = require('@adempiere/grpc-api')
-  let service = new ServiceApi(config)
+  const api = Router();
+  const ServiceApi = require('@adempiere/grpc-api/src/services/userInterface')
+  const service = new ServiceApi(config)
 
   /**
    * GET Report Output
